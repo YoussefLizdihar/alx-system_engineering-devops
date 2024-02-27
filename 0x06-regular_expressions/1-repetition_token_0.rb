8e535a7884b368925711a8ad7eb62ec30a9dbcc4
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-02-27 08:11:03 -0800

    {update 1}

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Search for Shcool
 
-puts ARGV[0].scan(/hbt[t+]?n/).join
+puts ARGV[0].scan(/hb(t{1,5})(?!t)n/).join
--- 0x06-regular_expressions/README.md
+++ 0x06-regular_expressions/README.md
@@ -1,3 +1,4 @@
 create 0x06-regular_expressions
 create 0-simply_match_school.rb
 create 1-repetition_token_0.rb
+up 1
