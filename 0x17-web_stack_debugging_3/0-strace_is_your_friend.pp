1d4fef9f197844fc06c51e2c22f0634d22736c71
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-06-05 19:55:59 -0700

    {up}

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+#fix error 500 using puppet
+
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
--- 0x17-web_stack_debugging_3/README.md
+++ 0x17-web_stack_debugging_3/README.md
@@ -1,3 +1,4 @@
 create 0x17-web_stack_debugging_3
 create 0
 up 0
+up 0
