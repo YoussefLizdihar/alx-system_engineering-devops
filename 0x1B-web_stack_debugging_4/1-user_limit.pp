eefad824ebf2b1801da53057992df432003f690e
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-06-13 07:40:45 -0700

    {update 1}

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,11 @@
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'increase-file-limits':
+  command => '/bin/bash -c "echo -e \'holberton soft nofile 4096\nholberton hard nofile 8192\' >> /etc/security/limits.conf"',
+  path    => ['/bin', '/usr/bin'],
+  unless  => '/bin/grep -q "holberton soft nofile" /etc/security/limits.conf',
 }
 
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'apply-sysctl':
+  command => '/sbin/sysctl -p',
+  path    => ['/sbin', '/bin', '/usr/bin'],
+  subscribe => Exec['increase-file-limits'],
 }
